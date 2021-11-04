f0e0b51da43867339bff4621cf665c5b690a8793
phit0 <philipp.schad@stud.uni-goettingen.de>
2021-11-04 23:50:04 +0100

    tests for the core calculations

---
+++ tests/testthat/test-ships.R
@@ -0,0 +1,19 @@
+
+datapath <- system.file("../ships.csv", package = "ships")
+test <- ships$new(datapath)
+
+test_that("Last distance entries of vessels are 0", {
+
+    expect_true(
+        all(test$data[, .SD[.N], by = .(SHIP_ID)
+        ][, .(loc_diff)] == 0)
+    )
+})
+
+test_that("Summarization preserves the correct information", {
+
+    expect_true(
+        all(do.call(paste0, test$summary[, .(SHIP_ID, LON1, LAT1, vessel_max)]) %in%
+                    do.call(paste0, test$data[, .(SHIP_ID, LON, LAT, loc_diff)]))
+        )
+})
