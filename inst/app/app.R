228b15000df4fdb68e85b5ac84ff6525dadd823d
phit0 <philipp.schad@stud.uni-goettingen.de>
2021-11-05 23:47:00 +0100

    make app prettier with shiny.semantic

--- inst/app/app.R
+++ inst/app/app.R
@@ -1,12 +1,21 @@
 library(shiny)
-library(ships)
+library(shiny.semantic)
+# library(ships)
+devtools::load_all(".")
 
-ui <- fluidPage(
-    #actionButton("load", "load app"),
-    uiOutput("vessel_type"),
-    uiOutput("ship_id"),
-    leaflet::leafletOutput("map")
-
+ui <- semanticPage(
+    titlePanel("Ship movements - December 2016", windowTitle = "ships"),
+    "Displayed is the longest distance between two recording positions for each vessel",
+    sidebar_layout(
+        sidebar_panel(
+            uiOutput("vessel_type"),
+            uiOutput("vessel_name"),
+            tableOutput("infoTab")
+        ),
+        main_panel(
+            leaflet::leafletOutput("map", height = 600)
+        )
+    )
 )
 
 server <- function(input, output, session) {
@@ -25,7 +34,7 @@ server <- function(input, output, session) {
         })
     })
 
-    # dynamic drop down menu for ship_id
+    # dynamic drop down menu for vessel_name
     observeEvent(input$vessel_type, {
         isolate({
             if (is.null(input$vessel_type)) {
@@ -35,20 +44,40 @@ server <- function(input, output, session) {
             }
         })
 
-        output$ship_id <- renderUI({
+        output$vessel_name <- renderUI({
 
-            selectInput("ship_id", "Choose Ship ID",
+            selectInput("vessel_name", "Choose Vessel",
                         choices = rv$shinyShip$summary[vessel_type == rv$vt,
-                                                       SHIP_ID],
+                                                       vessel_name],
                         selected = rv$shinyShip$summary[vessel_type == rv$vt,
-                                                        SHIP_ID][1])
+                                                        vessel_name][1])
         })
 
     })
 
-    observeEvent(req(input$ship_id, input$vessel_type), {
+    observeEvent(req(input$vessel_name, input$vessel_type), {
+        isolate({
+            sub <- rv$shinyShip$summary[
+                which(vessel_name == input$vessel_name,
+                      vessel_type == input$vessel_type),
+                ]
+            rv$info <- data.frame(
+                "." = c("ID", "Time", "Destination", "Speed", "Flag"),
+                "vessel info" = c(as.character(sub$SHIP_ID),
+                           paste0(round(sub$time[[1]] / 3600, 2), " h"),
+                           sub$DESTINATION,
+                           as.character(sub$SPEED),
+                           sub$FLAG)
+            )
+            colnames(rv$info)[1] <- ""
+        })
+        output$infoTab = renderTable({
+            rv$info
+        })
+
+
         output$map <- leaflet::renderLeaflet({
-            rv$shinyShip$display_ship(input$vessel_type, input$ship_id)
+            rv$shinyShip$display_ship(input$vessel_type, input$vessel_name)
         })
     })
 
